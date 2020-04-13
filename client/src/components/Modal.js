217fc22e466a0eb6c8ae4c6157e04b203851d8bb
andar <vitaliy.for@gmail.com>
2020-04-13 21:57:26 +0300

    - update react & react dom - added modal window

---
+++ client/src/components/Modal.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import ModalPanel from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '0',
+    left                  : '0',
+    right                 : '0',
+    bottom                : '0',
+    marginRight           : '0',
+  }
+};
+
+ModalPanel.setAppElement('#root')
+
+export const Modal = ({children, isOpen, onClose, onAfterOpen}) => {
+
+  return (
+    <ModalPanel
+      isOpen={isOpen}
+      onAfterOpen={() => console.log('on after open')}
+      onRequestClose={onClose}
+      style={customStyles}
+      contentLabel="Example Modal"
+      closeTimeoutMS={600}
+      className="app__modal"
+      // bodyOpenClassName={'body--modal-open'}
+      // htmlOpenClassName={'html--modal--open'}
+    >
+      { children }
+    </ModalPanel>
+  );
+};
--- client/src/pages/NewExpense.js
+++ client/src/pages/NewExpense.js
@@ -3,18 +3,32 @@ import { useHttp } from '../hooks/http.hook';
 import { AuthContext } from '../context/AuthContext';
 // import { AuthorContext } from './context/AuthorContext';
 import { useMessage } from '../hooks/message.hook';
+import { Loader } from "../components/Loader";
+import { Modal } from '../components/Modal';
+import ModalPanel from "react-modal";
+
 
 export const NewExpense = () => {
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState('');
   const [users, setUsers] = useState([]);
-  const [author, setAuthor] = useState('')
+  const [author, setAuthor] = useState('');
   const [borrowers, setBorrowers] = useState('');
   const [lenders, setLenders] = useState('');
-
+  const [modalIsOpen, setIsOpen] = useState(false);
   const { loading, request } = useHttp();
   const { token, userId } = useContext(AuthContext);
   const message = useMessage();
+
+  const openModal = (e) => {
+    e.preventDefault();
+    setIsOpen(true);
+  };
+
+  const closeModal = (e) => {
+    e.preventDefault();
+    setIsOpen(false);
+  };
 
   const getUsers = useCallback(async () => {
     try {
@@ -24,7 +38,7 @@ export const NewExpense = () => {
 
       setUsers(fetched);
     } catch (e) {
-      message(e.message);
+      message(e.message, 'error');
     }
   }, [token, request]);
 
@@ -38,7 +52,7 @@ export const NewExpense = () => {
     });
 
     if (authorData) {
-      setAuthor(authorData.name)
+      setAuthor(authorData.name);
     }
 
   }, [users]);
@@ -62,20 +76,24 @@ export const NewExpense = () => {
         },
         {
           Authorization: `Bearer ${token}`
-         }
+        }
       );
 
       message(data.message);
 
     } catch (e) {
-      message(e.message);
+      message(e.message, 'error');
     }
   };
+
+  if (loading) {
+    return <Loader/>;
+  }
 
   return (
     <div className="expense-create">
       <form className="expense-create__form form">
-        <div className="form__title" >Укажите название и сумму новой оплаты:</div>
+        <div className="form__title">Укажите название и сумму новой оплаты:</div>
 
         <div className="form__group">
           <div className="form__row">
@@ -108,20 +126,33 @@ export const NewExpense = () => {
         <div>
           <ul>
             {
-            users.map((user) =>
-              <li key={user._id}>
-                {user.name}
-              </li>
-            )}
+              users.map((user) =>
+                <li key={user._id}>
+                  {user.name}
+                </li>
+              )}
           </ul>
 
 
-          Платил <span>{ author }</span> и разделить <span>Поровну </span>
+          Платил <span>{author}</span> и разделить <span>Поровну </span>
         </div>
+
 
         <div className="form__btn-group">
           <a href="#" className="form__btn" onClick={onHandler}>Сохранить</a>
         </div>
+
+        <button onClick={e => openModal(e)}>Open Modal</button>
+        <Modal
+          onOpen={e => openModal(e)}
+          onClose={e => closeModal(e)}
+          isOpen={modalIsOpen}
+        >
+          <div>
+            <button onClick={e => closeModal(e)}>close</button>
+            see a'm a modal window. Lovely
+          </div>
+        </Modal>
       </form>
     </div>
   );
